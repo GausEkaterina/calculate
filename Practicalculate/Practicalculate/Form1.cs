1dfb869ca1d92b555e958e3e81bc7c1893247d78
GausEkaterina <86595316+GausEkaterina@users.noreply.github.com>
2021-07-01 14:26:38 +0600

    Summary

--- Practicalculate/Practicalculate/Form1.cs
+++ Practicalculate/Practicalculate/Form1.cs
@@ -12,9 +12,91 @@ namespace Practicalculate
 {
     public partial class Form1 : Form
     {
+
+        double digit, digit2, result;
+        int count;
+        bool sign = true;
+        private bool dot;
         public Form1()
         {
             InitializeComponent();
+        }
+        static class Class1
+        {
+            public static calculator2 cal2(int count)
+            {
+                switch (count)
+                {
+                    case 1:
+                        return new PLUS();
+                    case 2:
+                        return new MINUS();
+                    case 3:
+                        return new DIVITION();
+                    case 4:
+                        return new MULTIPLY();
+                    default:
+                        return new PLUS();
+
+                }
+
+            }
+        }
+        interface calculator2
+        {
+            double Calculate(double firstValue, double secondValue);
+        }
+        public class PLUS : calculator2
+        {
+            public double Calculate(double firstValue, double secondValue)
+            {
+                return (firstValue + secondValue);
+            }
+        }
+        public class MINUS : calculator2
+        {
+            public double Calculate(double firstValue, double secondValue)
+            {
+                return (firstValue - secondValue);
+            }
+        }
+        public class DIVITION : calculator2
+        {
+            public double Calculate(double firstValue, double secondValue)
+            {
+                return (firstValue / secondValue);
+            }
+        }
+        public class MULTIPLY : calculator2
+        {
+            public double Calculate(double firstValue, double secondValue)
+            {
+                return (firstValue * secondValue);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
